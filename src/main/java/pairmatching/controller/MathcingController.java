4b4c0ef758bff20871a26d7984ef1292387ee0fa
SEUNHGHEE96 <dnffktmdgml@gmail.com>
2022-12-09 15:34:55 +0900

    feat: Q 입력시 기능 중지

--- src/main/java/pairmatching/controller/MathcingController.java
+++ src/main/java/pairmatching/controller/MathcingController.java
@@ -1,14 +1,26 @@
 package pairmatching.controller;
 
 import pairmatching.model.Course;
+import pairmatching.model.Crew;
 import pairmatching.view.InputView;
 
 public class MathcingController {
     InputView inputView = new InputView();
+    Crew crew = new Crew();
     public void start() {
-        inputView.getMenu();
-        inputView.getScenarios();
+        crew.getNames();
+        String menu = inputView.getMenu();
+        while(true){
+            if(menu.equals("1")){
+                inputView.getScenarios();
+            } else if(menu.equals("2")){
+                inputView.getScenarios();
+            } else if(menu.equals("3")){
 
+            } else if(menu.equals("Q")){
+                break;
+            }
+        }
     }
 
 }
